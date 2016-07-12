0e085d7788309fabc0632c15aa71dca095e72584
KanamuHiguchi <www.nanasi.com@gmail.com>
2016-07-12 12:06:25 +0900

    衝突判定の追加

--- src/app.js
+++ src/app.js
@@ -6,59 +6,147 @@ var mylabel;
 var gameLayer;
 var background;
 var scrollSpeed = 1;
+//宇宙船で追加した部分　重力
+var ship;
+var gameGravity = -0.05;
+//宇宙船を操作するで追加した部分 エンジンの推進力
+var gameThrust = 0.1;
+
 
 var gameScene = cc.Scene.extend({
-    onEnter:function () {
-      //  var gradient = cc.LayerGradient.create(cc.color(0, 0, 0, 255), cc.color(0x46, 0x82, 0xB4, 255));
-      //  this.addChild(gradient);
-        this._super();
-        gameLayer = new game();
-        gameLayer.init();
-        this.addChild(gameLayer);
-    }
+  onEnter: function() {
+    //  var gradient = cc.LayerGradient.create(cc.color(0, 0, 0, 255), cc.color(0x46, 0x82, 0xB4, 255));
+    //  this.addChild(gradient);
+    this._super();
+    gameLayer = new game();
+    gameLayer.init();
+    this.addChild(gameLayer);
+  }
 });
 
 var game = cc.Layer.extend({
-    init:function () {
-        this._super();
-        size = cc.director.getWinSize();
-        // mylabel = cc.LabelTTF.create("GO!", "Arial", "32");
-        // mylabel.setPosition(size.width / 2, size.height / 2);
-        // this.addChild(mylabel);
+  init: function() {
+    this._super();
+    size = cc.director.getWinSize();
+    // mylabel = cc.LabelTTF.create("GO!", "Arial", "32");
+    // mylabel.setPosition(size.width / 2, size.height / 2);
+    // this.addChild(mylabel);
 
-        //スクロールする背景スプライトをインスタンス　スクロール速度:scrollSpeed
-        background = new ScrollingBG();
-        this.addChild(background);
-        //scheduleUpdate関数は、描画の都度、update関数を呼び出す
-        this.scheduleUpdate();
+    //宇宙船を操作するで追加した部分
+    cc.eventManager.addListener({
+      event: cc.EventListener.MOUSE,
+      onMouseDown: function(event) {
+        ship.engineOn = true;
+      },
+      onMouseUp: function(event) {
+        ship.engineOn = false;
+      }
+    }, this)
 
-    },
-    update:function(dt){
-      //backgroundのscrollメソッドを呼び出す
-        background.scroll();
+    //スクロールする背景スプライトをインスタンス　スクロール速度:scrollSpeed
+    background = new ScrollingBG();
+    this.addChild(background);
 
-    },
+    ship = new Ship();
+    this.addChild(ship);
+
+    //scheduleUpdate関数は、描画の都度、update関数を呼び出す
+    this.scheduleUpdate();
+    //小惑星の生成で追加
+    this.schedule(this.addAsteroid, 0.5);
+
+  },
+  update: function(dt) {
+    //backgroundのscrollメソッドを呼び出す
+    background.scroll();
+    ship.updateY();
+  },
+  //小惑星の生成で追加
+  addAsteroid: function(event) {
+    var asteroid = new Asteroid();
+    this.addChild(asteroid);
+  },
+  removeAsteroid: function(asteroid) {
+    this.removeChild(asteroid);
+  },
 
 });
 //スクロール移動する背景クラス
 var ScrollingBG = cc.Sprite.extend({
-    //ctorはコンストラクタ　クラスがインスタンスされたときに必ず実行される
-    ctor:function() {
-        this._super();
-        this.initWithFile(res.background_png);
-    },
-    //onEnterメソッドはスプライト描画の際に必ず呼ばれる
-    onEnter:function() {
-      //背景画像の描画開始位置 横960の画像の中心が、画面の端に設置される
-      this.setPosition(size.width,size.height /2 );
-      //  this.setPosition(480,160);
-    },
-    scroll:function(){
-      //座標を更新する
-        this.setPosition(this.getPosition().x-scrollSpeed,this.getPosition().y);
-        //画面の端に到達したら反対側の座標にする
-        if(this.getPosition().x<0){
-            this.setPosition(this.getPosition().x+480,this.getPosition().y);
-        }
+  //ctorはコンストラクタ　クラスがインスタンスされたときに必ず実行される
+  ctor: function() {
+    this._super();
+    this.initWithFile(res.background_png);
+  },
+  //onEnterメソッドはスプライト描画の際に必ず呼ばれる
+  onEnter: function() {
+    //背景画像の描画開始位置 横960の画像の中心が、画面の端に設置される
+    this.setPosition(size.width, size.height / 2);
+    //  this.setPosition(480,160);
+  },
+  scroll: function() {
+    //座標を更新する
+    this.setPosition(this.getPosition().x - scrollSpeed, this.getPosition().y);
+    //画面の端に到達したら反対側の座標にする
+    if (this.getPosition().x < 0) {
+      this.setPosition(this.getPosition().x + 480, this.getPosition().y);
     }
+  }
 });
+
+//重力（仮）で落下する　宇宙船　
+var Ship = cc.Sprite.extend({
+  ctor: function() {
+    this._super();
+    this.initWithFile(res.ship_png);
+    this.ySpeed = 0; //宇宙船の垂直速度
+    //宇宙船を操作するで追加した部分
+    this.engineOn = false; //カスタム属性追加　宇宙船のエンジンのON OFF
+  },
+  onEnter: function() {
+    this.setPosition(60, 160);
+  },
+  updateY: function() {
+    //宇宙船を操作するで追加した部分
+    if (this.engineOn) {
+      this.ySpeed += gameThrust;
+    }
+    //ここまで
+    this.setPosition(this.getPosition().x, this.getPosition().y + this.ySpeed);
+    this.ySpeed += gameGravity;
+  }
+});
+//小惑星クラス
+var Asteroid = cc.Sprite.extend({
+  ctor: function() {
+    this._super();
+    this.initWithFile(res.asteroid_png);
+  },
+  onEnter: function() {
+    this._super();
+    this.setPosition(600, Math.random() * 320);
+    var moveAction = cc.MoveTo.create(2.5, new cc.Point(-100, Math.random() * 320));
+    this.runAction(moveAction);
+    this.scheduleUpdate();
+  },
+  update: function(dt) {
+    //小惑星との衝突を判定する処理
+    var shipBoundingBox = ship.getBoundingBox();
+    var asteroidBoundingBox = this.getBoundingBox();
+		//rectIntersectsRectは２つの矩形が交わっているかチェックする
+    if (cc.rectIntersectsRect(shipBoundingBox, asteroidBoundingBox) ) {
+      gameLayer.removeAsteroid(this);//小惑星を削除する
+      restartGame();
+    }
+		//画面の外にでた小惑星を消去する処理
+    if (this.getPosition().x < -50) {
+      gameLayer.removeAsteroid(this)
+    }
+  }
+});
+//宇宙船を元の位置に戻して、宇宙船の変数を初期化する
+function restartGame(){
+    ship.ySpeed = 0;
+    ship.setPosition(ship.getPosition().x,160);
+    ship.invulnerability=100;
+}
